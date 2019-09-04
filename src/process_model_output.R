3781ebb14ec25d9436c0bed9cbfce4d8381ae0cc
Platt <lplatt@usgs.gov>
2019-09-04 14:52:27 -0500

    make the fake data a bit more realistic by using precip data instead of randomized categories

--- src/process_model_output.R
+++ src/process_model_output.R
@@ -6,9 +6,12 @@ hru_ids <- ncvar_get(nc, varid = "hruid")
 #could verify time axis matches expected date
 #generate random labels for now
 #actual metric computation happens here eventually
-categories <- rep(c("low", "medium", "high"), length.out = length(hru_ids))
+vals <- ncvar_get(nc, varid = "prcp")[,1]
+vals_without_zeros <- vals[vals != 0]
+percentiles <- quantile(vals_without_zeros, probs = c(0.10, 0.30, 0.70, 0.90, 1))
+percentiles <- c(0, percentiles)
+categories <- c("very low", "low", "medium", "high", "very high")
+vals_categorized <- cut(vals, breaks = percentiles, labels = categories, include.lowest = TRUE)
 data_vals <- dplyr::tibble(hru_id_nat = hru_ids, 
-                    value = categories)
+                           value = as.character(vals_categorized))
 readr::write_csv(data_vals, "model_output_categorized.csv")
-
-
