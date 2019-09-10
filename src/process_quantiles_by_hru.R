4f27bf70737ad1b14241744771630379447a8ad3
Platt <lplatt@usgs.gov>
2019-09-10 13:30:34 -0500

    get slurm job to run!

--- src/process_quantiles_by_hru.R
+++ src/process_quantiles_by_hru.R
@@ -23,6 +23,7 @@ calcuate_percentiles_by_hru <- function() {
   
   # Read in data that has all data for all HRUs
   # This is going to be a massive file.
+  message("Reading in data for all HRUs.")
   variable_df <- readRDS("combined_vars.rds")
   
   #from yeti environment
@@ -33,37 +34,38 @@ calcuate_percentiles_by_hru <- function() {
   col_id_start <- hru_id_start + 1 # adds one for the date column
   col_id_end <- col_id_start + 24 # gives location of final col
   # If we are past the last column, cut the final col id off
-  col_id_end <- ifelse(col_id_end > ncol(variable_df), 
+  col_id_end <- ifelse(col_id_end > ncol(variable_df),
                        yes = ncol(variable_df),
                        no = col_id_end)
-  
+
   # Isolate data for single HRU
   columns_to_select <- c(1, col_id_start:col_id_end)
   task_df <- variable_df[, .SD, .SDcols = columns_to_select]
-  
+
   # Sum rows to get the "total storage" value for each day
   total_storage_task_df <- task_df[, lapply(.SD, sum), by = Date]
-  
-  # Now calculate quantiles for each DOY using a moving window
-  # centered at the current DOY
-  total_storage_task_df[, DOY := as.numeric(format(Date, "%j"))] 
 
-  # Do this for each HRU column
+  # Add column with numeric DOY
+  total_storage_task_df[, DOY := as.numeric(format(Date, "%j"))]
+
+  # Calculate quantiles for each DOY using a moving window
+  # centered at the current DOY for each HRU column
+  message("Calculating quantiles for each HRU in this set.")
   hrus <- names(total_storage_task_df)[-grep("DOY|Date", names(total_storage_task_df))]
   hru_quantile_list <- lapply(hrus, function(hru_id) {
-    
+
     # Subset to just Date and this HRUs column
     hru_df <- select(total_storage_task_df, Date, DOY, matches(hru_id))
-    
+
     # doy = current day of year as number
-    # df = data frame with all data, assumes cols DOY and 
+    # df = data frame with all data, assumes cols DOY and
     # window = number of days to look behind and ahead of the current doy
     get_quantiles_doy <- function(doy, df, hru_id, window = 5) {
-      
+
       # Determine which doy to start & end with in the window
       begin_doy <- doy - window
       end_doy <- doy + window
-      
+
       # Handle edge cases:
       if(begin_doy < 0) {
         # Need to look backward into last days of the year
@@ -74,35 +76,34 @@ calcuate_percentiles_by_hru <- function() {
       } else {
         seq_doy <- begin_doy:end_doy
       }
-      
+
       # Filter giant dataset to just those with the right days
       values_to_calc_df <- filter(df, DOY %in% seq_doy)
-      doy_quantiles <- quantile(values_to_calc_df[[hru_id]], 
+      doy_quantiles <- quantile(values_to_calc_df[[hru_id]],
                                 probs = c(0, 0.10, 0.25, 0.75, 0.90, 1))
-      
+
       return(list(doy_quantiles))
     }
-    
-    hru_quantiles <- hru_df %>% 
-      rowwise() %>% 
-      mutate(DOY_Quantiles = get_quantiles_doy(DOY, hru_df, hru_id)) %>% 
-      select(DOY, DOY_Quantiles) %>% 
-      unique() %>% 
+
+    hru_quantiles <- hru_df %>%
+      rowwise() %>%
+      mutate(DOY_Quantiles = get_quantiles_doy(DOY, hru_df, hru_id)) %>%
+      select(DOY, DOY_Quantiles) %>%
+      unique() %>%
       ungroup()
-    
+
     # Rename the second column (which is always "DOY_QUANTILES" based on the select above)
     names(hru_quantiles)[2] <- hru_id
-    
+
     return(hru_quantiles)
   })
-  
+
   # Combine data frames of HRU quantiles into one by joining on DOY
-  total_storage_quantiles <- hru_quantile_list %>% 
+  total_storage_quantiles <- hru_quantile_list %>%
     reduce(left_join, by = "DOY")
-  
-  saveRDS(total_storage_quantiles, 
-          sprintf("%stotal_storage_quantiles_%s.rds", 
-                  file_location, 
+
+  saveRDS(total_storage_quantiles,
+          sprintf("quantiles_by_task/total_storage_quantiles_%s.rds",
                   task_id))
   return(total_storage_quantiles)
 }
