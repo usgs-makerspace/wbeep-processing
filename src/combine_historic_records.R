7bc0469222ff8dba94f4d0262ff64418339d4710
Platt <lplatt@usgs.gov>
2019-09-11 13:38:53 -0500

    remove option to use CSV files, only NetCDF

--- src/combine_historic_records.R
+++ src/combine_historic_records.R
@@ -1,34 +1,27 @@
-library(data.table)
 library(dplyr)
 library(ncdf4) 
 
-read_period_of_record_data <- function(fn, varid = NULL) {
+read_period_of_record_data <- function(fn, varid) {
   
-  if(is.null(varid)) {
-    # The file is a CSV
-    data <- fread(fn, header=TRUE)
-    data[, Date := as.Date(Date)]
-  } else {
-    # The file is NetCDF
-    nc <- nc_open(sprintf("historical_%s_out.nc", varid))
-    
-    message(sprintf("Reading in NetCDF data for %s", varid))
-    data_nc <- ncvar_get(nc, varid)
-    time <- ncvar_get(nc, "time")
-    hruid <- ncvar_get(nc, "hruid")
-    
-    # Convert ncdf4 times to R dates
-    time_att <- ncdf4::ncatt_get(nc, "time")
-    time_start <- as.Date(gsub("days since ", "", time_att$units))
-    time_fixed <- time + time_start # creates dates from the "days since" var
-    
-    # The data is actually transformed and missing hruid and time
-    message(sprintf("Transforming data & adding time/hruid attributes for %s", varid))
-    data_transformed <- t(data_nc)
-    data <- cbind(time_fixed, data_transformed)
-    names(data) <- c("Date", hruid)
-  }
+  # The file is NetCDF
+  nc <- nc_open(sprintf("historical_%s_out.nc", varid))
   
+  message(sprintf("Reading in NetCDF data for %s", varid))
+  data_nc <- ncvar_get(nc, varid)
+  time <- ncvar_get(nc, "time")
+  hruid <- ncvar_get(nc, "hruid")
+  
+  # Convert ncdf4 times to R dates
+  time_att <- ncdf4::ncatt_get(nc, "time")
+  time_start <- as.Date(gsub("days since ", "", time_att$units))
+  time_fixed <- time + time_start # creates dates from the "days since" var
+  
+  # The data is actually transformed and missing hruid and time
+  message(sprintf("Transforming data & adding time/hruid attributes for %s", varid))
+  data_transformed <- t(data_nc)
+  data <- cbind(time_fixed, data_transformed)
+  names(data) <- c("Date", hruid)
+
   return(data)
 }
 
@@ -42,16 +35,6 @@ combine_variables_to_one_df <- function(dflist) {
   
   return(variable_df)
 }
-
-# Read the data sources ----
-
-# ScienceBase CSV version
-# soil_moist_tot <- read_period_of_record_data("nhru_soil_moist_tot.csv")
-# pkwater_equiv <- read_period_of_record_data("nhru_pkwater_equiv.csv")
-# hru_intcpstor <- read_period_of_record_data("nhru_hru_intcpstor.csv")
-# hru_impervstor <- read_period_of_record_data("nhru_hru_impervstor.csv")
-# gwres_stor <- read_period_of_record_data("nhru_gwres_stor.csv")
-# dprst_stor <- read_period_of_record_data("nhru_dprst_stor_hru.csv")
 
 # NetCDF files from Steve Markstrom
 # soil_moist_tot <- read_period_of_record_data("historical_soil_moist_tot_out.nc", "soil_moist_tot")
