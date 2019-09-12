7e1dae0c68e9d1edbbda62f2df90ab8c9ef76152
Platt <lplatt@usgs.gov>
2019-09-11 20:45:34 -0500

    combining data ran out of space, so set it up to happen in chunks

--- src/combine_historic_records.R
+++ src/combine_historic_records.R
@@ -1,8 +1,6 @@
 library(dplyr)
 library(ncdf4) 
 
-message(sprintf("Started at %s", Sys.time()))
-
 read_period_of_record_data <- function(fn, varid) {
   
   # The file is NetCDF
@@ -38,25 +36,48 @@ combine_variables_to_one_df <- function(dflist) {
   return(variable_df)
 }
 
+message(sprintf("Started first set at %s", Sys.time()))
+
 # NetCDF files from Steve Markstrom
 soil_moist_tot <- read_period_of_record_data("historical_soil_moist_tot_out.nc", "soil_moist_tot")
 pkwater_equiv <- read_period_of_record_data("historical_pkwater_equiv_out.nc", "pkwater_equiv")
 hru_intcpstor <- read_period_of_record_data("historical_hru_intcpstor_out.nc", "hru_intcpstor")
+
+variable_df_list1 <- list(
+  pkwater_equiv = pkwater_equiv,
+  soil_moist_tot = soil_moist_tot,
+  hru_intcpstor = hru_intcpstor
+)
+
+# Combine all variables into one df ----
+variable_df1 <- combine_variables_to_one_df(variable_df_list1)
+saveRDS(variable_df1, "combined_vars1.rds")
+rm(soil_moist_tot, pkwater_equiv, hru_intcpstor, variable_df_list1, variable_df1)
+
+message(sprintf("Finished first set at %s", Sys.time()))
+
+# Have to do in two different sets because of memory issues
+message(sprintf("Started second set at %s", Sys.time()))
+
 hru_impervstor <- read_period_of_record_data("historical_hru_impervstor_out.nc", "hru_impervstor")
 gwres_stor <- read_period_of_record_data("historical_gwres_stor_out.nc", "gwres_stor")
 dprst_stor <- read_period_of_record_data("historical_dprst_stor_hru_out.nc", "dprst_stor_hru")
 
-variable_df_list <- list(
-  pkwater_equiv = pkwater_equiv,
-  soil_moist_tot = soil_moist_tot,
-  hru_intcpstor = hru_intcpstor,
+variable_df_list2 <- list(
   hru_impervstor = hru_impervstor,
   gwres_stor = gwres_stor,
   dprst_stor = dprst_stor
 )
 
 # Combine all variables into one df ----
-variable_df <- combine_variables_to_one_df(variable_df_list)
-saveRDS(variable_df, "combined_vars.rds")
+variable_df2 <- combine_variables_to_one_df(variable_df_list2)
+saveRDS(variable_df2, "combined_vars2.rds")
+rm(hru_impervstor, gwres_stor, dprst_stor, variable_df_list2)
+message(sprintf("Finished second set at %s", Sys.time()))
 
-message(sprintf("Finished at %s", Sys.time()))
+message("Read first set df")
+variable_df1 <- readRDS("combined_vars1.rds")
+
+message("Combine all data and save")
+combined_vars <- bind_rows(variable_df1, variable_df2)
+saveRDS(combined_vars, "combined_vars_all.rds")
