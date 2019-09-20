e953540c695ea1a0f308277d3289de1c92400a3b
Megan Hines <mhines@usgs.gov>
2019-09-20 10:52:52 -0500

    Try gtools::smartbind
    ignores header names which was an issue with rbind

--- src/combine_hru_quantiles.R
+++ src/combine_hru_quantiles.R
@@ -4,15 +4,13 @@
 library(dplyr)
 library(purrr) # needed for `reduce`
 library(tidyr)
+library(gtools)
 
 files_to_combine <- list.files(path = "quantiles_by_hru", 
                                pattern = "total_storage_quantiles",
                                full.names = TRUE)
-list_of_files <- lapply(files_to_combine, readRDS)
-#combined_data <- reduce(list_of_files, left_join)
-#combined_data_reformatted <- gather(combined_data, key = hruid, value = total_storage_quantiles, -DOY)
-combined_data <- do.call('rbind', lapply(files_to_combine), readRDS)
-combined_data_reformatted <- gather(combined_data, key = hruid, value = total_storage_quantiles, -DOY)
+stack <- do.call("smartbind", lapply(files_to_combine, readRDS))
+combined_data_reformatted <- gather(stack, key = hruid, value = total_storage_quantiles, -DOY)
 
 saveRDS(combined_data_reformatted, "all_quantiles.rds")
 ## manually push RDS file to S3
