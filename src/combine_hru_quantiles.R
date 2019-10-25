7598f58c66f88af416d46e7f6d876635874488cc
Platt <lplatt@usgs.gov>
2019-10-25 15:53:22 -0500

    add messages in combine quantiles step

--- src/combine_hru_quantiles.R
+++ src/combine_hru_quantiles.R
@@ -9,6 +9,7 @@ files_to_combine <- list.files(path = "grouped_quantiles",
                                pattern = "total_storage_quantiles",
                                full.names = TRUE)
 #gets daily data
+message("Read in each HRU group of quantiles file to a list")
 file_contents_list <- lapply(files_to_combine, feather::read_feather)
 # print(proc.time() - ptm)
 # ptm <- proc.time()
@@ -23,6 +24,7 @@ file_contents_list <- lapply(files_to_combine, feather::read_feather)
 # ptm <- proc.time()
 # stopifnot(all(checks))
 
+message("Turn each HRU group of quantiles from list into single dataset")
 combined_data <- do.call("rbind", file_contents_list)# %>%  
 #   select(DOY, matches('^[0-9]*$')) %>% 
 #   pivot_longer(cols = -DOY, names_to = "hruid",
@@ -37,6 +39,7 @@ combined_data <- do.call("rbind", file_contents_list)# %>%
 # combined_data[,total_storage_quantiles := NULL]
 
 # Add -inf and +inf here since it will be the same for all 
+message("Fix quantiles to add 0% and 100%")
 original_quantiles <- tail(names(combined_data), -2)
 combined_data <- as.data.table(combined_data)
 combined_data[, "0%" := -Inf]
@@ -45,5 +48,6 @@ setcolorder(combined_data, c("hruid", "DOY", "0%",
                              original_quantiles,
                              "100%"))
 
+message("Save quantiles `as all_quantiles.rds`")
 saveRDS(combined_data, "all_quantiles.rds")
 ## manually push RDS file to S3
