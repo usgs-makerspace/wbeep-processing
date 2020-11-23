78588240464f2608e6b96ea0931aca9d7b063f1c
mhines-usgs <mhines@usgs.gov>
2020-11-23 10:28:43 -0600

    more edits for new data

--- src/validate_total_storage_categorized.R
+++ src/validate_total_storage_categorized.R
@@ -4,7 +4,7 @@ validate_total_storage_categorized <- function(data_categorized, n_hrus = 114958
   
   ##### Test: Key columns exist in the data #####
   
-  assert_that("hru_id_nat" %in% names(data_categorized))
+  assert_that("nhru" %in% names(data_categorized))
   assert_that("value" %in% names(data_categorized))
   
   ##### Test: All HRUs are in the data and in the right order #####
@@ -17,8 +17,8 @@ validate_total_storage_categorized <- function(data_categorized, n_hrus = 114958
   # We know that one CA HRU doesn't have historic data for quantiles (104388) and will be Undefined
   # This is also true for 7 other HRUs that are not in the U.S.
   #problem_hruids <- c(104388, 46760, 46766, 46767, 82924, 82971, 82983, 82984)
-  #data_categorized_problem_hru <- data_categorized[data_categorized$hru_id_nat %in% problem_hruids,]
-  #data_categorized_good_hrus <- data_categorized[!data_categorized$hru_id_nat %in% problem_hruids,]
+  #data_categorized_problem_hru <- data_categorized[data_categorized$nhru %in% problem_hruids,]
+  #data_categorized_good_hrus <- data_categorized[!data_categorized$nhru %in% problem_hruids,]
   
   assert_that(is.character(data_categorized$value))
   #assert_that(all(data_categorized_problem_hru$value == "Undefined"))
