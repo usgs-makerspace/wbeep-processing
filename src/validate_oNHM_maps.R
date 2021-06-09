331171e620dab791b14d03822dcb81c488e6dc3c
mhines-usgs <mhines@usgs.gov>
2021-06-09 16:23:41 -0500

    modifying categorization tests to warn instead of error, improved report appearance and content

--- src/validate_oNHM_maps.R
+++ src/validate_oNHM_maps.R
@@ -1,7 +1,7 @@
 library(sf)
 library(ggplot2)
 
-validate_oNHM_maps <- function(bad_data_hruids, mapfilename, var, title) {
+validate_oNHM_maps <- function(bad_data_hruids, mapfilename, title) {
   
   # Read in the shapefiles for hrus and background states layer so we can map the hrus that are flagged
   hrus <- read_sf(dsn=".",layer="hrus")
@@ -17,11 +17,11 @@ validate_oNHM_maps <- function(bad_data_hruids, mapfilename, var, title) {
   
   ggplot() + 
     geom_sf(data = states_proj, size=1, color="gray", fill="white") +
-    geom_sf(data = hrus_to_plot_proj, size = 2, color = "red") + 
+    geom_sf(data = hrus_to_plot_proj, size = 2, color = "#FC4E07") + 
     geom_sf_text(data = hrus_to_plot_proj,
                  aes(label = nhru_v1_1),
                  size=3,
-                 color="blue", check_overlap = TRUE) +
+                 color="#293352", check_overlap = TRUE) +
     ggtitle(sprintf("%s", title)) + 
     theme_void() +
     theme(plot.margin=grid::unit(c(0,0,0,0), "in")) +
--- src/validate_total_storage_categorized.R
+++ src/validate_total_storage_categorized.R
@@ -1,17 +1,25 @@
 library(assertthat)
 
-validate_total_storage_categorized <- function(data_categorized, n_hrus = 114958) {
+validate_total_storage_categorized <- function(data_categorized, validate_fn, n_hrus = 114958) {
+  
+  write(x = sprintf("<h2>Validating the total storage categorization</h2>"),
+        file = validate_fn,
+        append = TRUE)
   
   ##### Test: Key columns exist in the data #####
   
   assert_that("hru_id_nat" %in% names(data_categorized))
   assert_that("value" %in% names(data_categorized))
-  
+  write(x = sprintf("<img src='icon-check.png' alt='test passed'> Key columns exist in the data <br />"), 
+        file = validate_fn,
+        append = TRUE)
   ##### Test: All HRUs are in the data and in the right order #####
   
   assert_that(nrow(data_categorized) == n_hrus)
   assert_that(all(data_categorized$hru_id_nat == 1:n_hrus))
-  
+  write(x = sprintf("<img src='icon-check.png' alt='test passed'> All HRUs are in the data and in the right order <br />"), 
+        file = validate_fn,
+        append = TRUE)
   ##### Test: Only expected categories exist in the value column #####
   
   # In case we are missing historic data for quantiles their daily data will be Undefined
@@ -20,7 +28,26 @@ validate_total_storage_categorized <- function(data_categorized, n_hrus = 114958
   data_categorized_good_hrus <- data_categorized[!data_categorized$hru_id_nat %in% problem_hruids,]
   
   assert_that(is.character(data_categorized$value))
-  assert_that(all(data_categorized_problem_hru$value == "Undefined"))
-  assert_that(!"Undefined" %in% unique(data_categorized_good_hrus$value))
+  # Use validate_that instead of assert_that so we get a warning instead of an error
+  validate_that(all(data_categorized_problem_hru$value == "Undefined"), msg = "All the HRUs categorized as problems are correctly categorized as Undefined.")
+  validate_that(!"Undefined" %in% unique(data_categorized_good_hrus$value), msg = "There are unexpected/new HRUs with the categorization of Undefined. ")
+  all_new_problem_hrus <- values_categorized[which(values_categorized$value=='Undefined'),]
+  all_new_problem_hrus <- all_new_problem_hrus$hru_id_nat
+  newest_problem_hrus <- all_new_problem_hrus[!all_new_problem_hrus %in% problem_hruids]
   
+  if (length(all_new_problem_hrus)>0) {
+    write(x = sprintf("<img src='icon-x.png' alt='test failed - warning'> There are unexpected/new HRUs with the categorization of Undefined. <br />"), 
+        file = validate_fn,
+        append = TRUE)
+    write(x=sprintf("<img src='icon-x.png' alt='test failed - warning'> We have new problem HRUs for today, unexpected/new Undefined categorization for the following HRUs: %s <br />", newest_problem_hrus),
+        file = validate_fn,
+        append = TRUE)
+    maptitle <- paste0("new_undefined_",today,".png")
+    validate_oNHM_maps(newest_problem_hrus, maptitle, "new problem/undefined HRUs for today")
+    write(x = sprintf("<a href='%s' target='_blank'><img src='%s' alt='map showing hrus that have an unexpected/new categorization of Undefined'></a><br />", maptitle, maptitle), 
+        file = validate_fn,
+        append = TRUE)
+  } else {
+    write(x = sprintf("<img src=icon-check.png' alt='test passed'>All HRUs categorized as problems are correctly categorized as Undefined and no new/unexpected Undefined values have been found. <br />"))
+  }
 }
